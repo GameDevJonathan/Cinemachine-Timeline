1a620bf2af4a01d5506ad71d9c527623e251e4c7
GameDevJonathan <jfagan16@gmail.com>
2023-09-16 13:55:05 -0400

    last body cameras

--- CinemachineTimeline/Assets/Course/Scripts/CameraSwitch.cs
+++ CinemachineTimeline/Assets/Course/Scripts/CameraSwitch.cs
@@ -5,20 +5,59 @@ using UnityEngine;
 public class CameraSwitch : MonoBehaviour
 {
     [SerializeField] private GameObject[] cameras;
+    [SerializeField] private int _cameraIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R))
+        if (_cameraIndex > cameras.Length - 1)
+            _cameraIndex = 0;
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        _cameraIndex++;
+
+        switch (_cameraIndex)
         {
-            cameras[0].SetActive(false);
-            cameras[1].SetActive(false);
+            case 0:
+                cameras[0].gameObject.SetActive(true);
+                cameras[1].gameObject.SetActive(false);
+                cameras[2].gameObject.SetActive(false);
+                cameras[3].gameObject.SetActive(false);
+                break;
+
+            case 1:
+                cameras[0].gameObject.SetActive(false);
+                cameras[1].gameObject.SetActive(true);
+                cameras[2].gameObject.SetActive(false);
+                cameras[3].gameObject.SetActive(false);
+                break;
+
+            case 2:
+                cameras[0].gameObject.SetActive(false);
+                cameras[1].gameObject.SetActive(false);
+                cameras[2].gameObject.SetActive(true);
+                cameras[3].gameObject.SetActive(false);
+                break;
+
+            case 3:
+                cameras[0].gameObject.SetActive(false);
+                cameras[1].gameObject.SetActive(false);
+                cameras[2].gameObject.SetActive(false);
+                cameras[3].gameObject.SetActive(true);
+                break;
+
+
         }
-        
+
+
+
     }
 }
