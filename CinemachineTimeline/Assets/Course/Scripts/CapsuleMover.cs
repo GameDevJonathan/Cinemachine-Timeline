e392e0ea4ba3a4404ef2696f663a532557cb3dd2
GameDevJonathan <jfagan16@gmail.com>
2023-10-11 14:58:03 -0400

    commiting

--- CinemachineTimeline/Assets/Course/Scripts/CapsuleMover.cs
+++ CinemachineTimeline/Assets/Course/Scripts/CapsuleMover.cs
@@ -8,6 +8,7 @@ public class CapsuleMover : MonoBehaviour
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _rotateSpeed;
     private Vector3 _direction;
+    private Vector2 _forwardDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +23,12 @@ public class CapsuleMover : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
             UnityEditor.EditorApplication.isPlaying = false ;
 #endif
+
         float _horizontal = Input.GetAxis("Horizontal");
         float _vertical = Input.GetAxis("Vertical");
         _direction = new Vector3(0, 0, _vertical);
         _direction *= _moveSpeed * Time.deltaTime;
+
         controller.Move(_direction);
 
         Vector3 rotation = new Vector3(0, _horizontal * _rotateSpeed * Time.deltaTime, 0);
