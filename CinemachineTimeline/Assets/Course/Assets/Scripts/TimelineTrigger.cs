f82f42e75f159699a483fff9b928c7c8aaf4bf27
GameDevJonathan <117468368+GameDevJonathan@users.noreply.github.com>
2023-11-27 16:18:49 -0500

    finished chest animation

--- CinemachineTimeline/Assets/Course/Assets/Scripts/TimelineTrigger.cs
+++ CinemachineTimeline/Assets/Course/Assets/Scripts/TimelineTrigger.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.InputSystem;
 
 public class TimelineTrigger : MonoBehaviour
 {
     [SerializeField] private PlayableDirector _playableDirector;
+    [SerializeField] private PlayerInput _playerInput;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player" )
-        {
+        if (other.tag == "Player" )        
             _playableDirector.Play();
-        }
+    }
+
+    public void StopInput()
+    {
+        if(_playableDirector.time == 0f)
+        _playerInput.enabled = false;
+    }
+
+    public void EnableInput()
+    {
+        if(_playableDirector.time >= 2.85f) 
+        _playerInput.enabled = true;
+
+        this.gameObject.SetActive(false);
     }
 
 }
